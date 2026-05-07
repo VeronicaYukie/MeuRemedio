522835ec30dc18a5d95a758b5eea63ec8d51e8b5
VeronicaYukie <veronica.ypires@gmail.com>
2026-05-07 00:28:39 -0300

    Teste 2 prontuário

--- AppLembreteMedicacao/MainPage.xaml.cs
+++ AppLembreteMedicacao/MainPage.xaml.cs
@@ -163,7 +163,7 @@ public partial class MainPage : ContentPage
             prontuario += $"💊 {m.Nome} ({m.Dosagem})\n";
         }
 
-        // --- AQUI ENTRA A SEGURANÇA (VERONICA) ---
+        /* --- AQUI ENTRA A SEGURANÇA (VERONICA) ---
         // Chamamos o SecurityHelper para proteger o texto
         string hashSeguro = SecurityHelper.GerarHash(prontuario);
 
@@ -174,7 +174,7 @@ public partial class MainPage : ContentPage
             Text = $"Hash de Segurança:\n{hashSeguro}",
             Uri = "App Meu Remédio"
 
-        });
+        });*/
         // ADICIONE ISSO ABAIXO DO SHARE:
         await DisplayAlert("Sucesso", "Compartilhamento concluído! Retornando ao início...", "OK");
     }
