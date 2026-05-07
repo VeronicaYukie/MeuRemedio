cf8482b55524cd1fdbf4b190f27d1a522af7d679
VeronicaYukie <veronica.ypires@gmail.com>
2026-05-07 00:52:12 -0300

    retirado security

--- AppLembreteMedicacao/Helpers/SecurityHelper.cs
+++ AppLembreteMedicacao/Helpers/SecurityHelper.cs
@@ -5,18 +5,3 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 
-namespace AppLembreteMedicacao.Helpers
-{
-public static class SecurityHelper
- 
-{ public static string GerarHash(string senha)
- 
-{
-  using var sha256 = SHA256.Create();
-  var bytes = Encoding.UTF8.GetBytes(senha);
-  var hash = sha256.ComputeHash(bytes);
-  return Convert.ToBase64String(hash);
-
-        }
-    }
-}
