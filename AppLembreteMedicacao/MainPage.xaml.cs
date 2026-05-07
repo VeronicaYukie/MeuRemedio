cf9c1c6da0aef64c2f37d7db84712c4f76edd5b3
VeronicaYukie <veronica.ypires@gmail.com>
2026-05-07 00:56:54 -0300

    Teste 6 Prontuário

--- AppLembreteMedicacao/MainPage.xaml.cs
+++ AppLembreteMedicacao/MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using AppLembreteMedicacao.Helpers;
+﻿
 using AppLembreteMedicacao.Models;
 using AppLembreteMedicacao.Views;
 using Plugin.LocalNotification;
@@ -127,7 +127,7 @@ public partial class MainPage : ContentPage
         }
     }
 
-   /* private async void OnDeleteClicked(object sender, EventArgs e)
+   private async void OnDeleteClicked(object sender, EventArgs e)
     {
         var med = (sender as Button).CommandParameter as Medicamento;
 
@@ -145,7 +145,7 @@ public partial class MainPage : ContentPage
 
    
 
-    private async void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    /*private async void ToolbarItem_Clicked_1(object sender, EventArgs e)
     {
         // 1. Busca os remédios salvos no banco SQLite configurado ontem
         var lista = await App.Banco.GetMedicamentos();
